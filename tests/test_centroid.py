88b765f82aad9a60dcbdd0111f737573c6f06e24
Daniel Ik <codingeinstein@gmail.com>
2024-08-08 23:08:44 +0100

    add tests

--- tests/test_centroid.py
+++ tests/test_centroid.py
@@ -1,5 +1,7 @@
 import unittest
 import torch
+from sklearn.datasets import make_blobs
+
 
 from torchclust import centroid
 
@@ -11,3 +13,10 @@ class TestCentroidBasedAlgorithms(unittest.TestCase):
         labels = kmeans.fit_predict(x)
 
         self.assertEqual(labels.shape[0], x.shape[0])
+
+    def test_kmeans_clustering(self):
+        x, labels = make_blobs(1000, n_features=3, centers=3)
+        dbscan = centroid.KMeans(num_clusters=3)
+        labels_ = dbscan.fit_predict(torch.from_numpy(x))
+
+        self.assertEqual(labels.shape[0], labels_.shape[0])
--- tests/test_density.py
+++ tests/test_density.py
@@ -1,5 +1,6 @@
 import unittest
 import torch
+from sklearn.datasets import make_blobs
 
 from torchclust import density
 
@@ -11,3 +12,10 @@ class TestDensityBasedAlgorithms(unittest.TestCase):
         labels = dbscan.fit_predict(x)
 
         self.assertEqual(labels.shape[0], x.shape[0])
+
+    def test_dbscan_clustering(self):
+        x, labels = make_blobs(1000, n_features=3, centers=3)
+        dbscan = density.DBSCAN(min_samples=10, eps=0.1)
+        labels_ = dbscan.fit_predict(torch.from_numpy(x))
+
+        self.assertEqual(labels.shape[0], labels_.shape[0])
