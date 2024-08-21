52b325964e70eb5b5558acb614c53820b7a42d05
Daniel Ik <codingeinstein@gmail.com>
2024-08-21 16:21:05 +0100

    Self organising maps

---
+++ torchclust/cluster/deep/som.py
@@ -0,0 +1,17 @@
+import torch
+
+class SelfOrganisingMap:
+    def __init__(self):
+        pass
+
+    @torch.no_grad()
+    def fit(self):
+        pass
+
+    @torch.no_grad()
+    def predict(self):
+        pass
+
+    @torch.no_grad()
+    def fit_predict(self):
+        pass
